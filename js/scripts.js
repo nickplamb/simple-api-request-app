b75be87216b51a9a026881e5e0516bb03a08303f
Nicholas Lamb <nickplamb@gmail.com>
2021-03-07 08:04:15 -0500

    add test array of trivia questions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,36 @@
-alert('Hello World!');
-
-let favoriteFood = 'Pho';
-
-document.write(`<h1>My favorite food is ${favoriteFood}.</h1>`);
+triviaRound = [
+  {
+    questionNumber: 1,
+    catagory: 'science',
+    question: 'What is the acceleration due to gravity on earth?',
+    possibleAnswers: [
+      '9.8 meters per second',
+      '9.8 meters per second squared',
+      '7.2 meters per second squared',
+      '7.2 meters per second'
+    ],
+    correctAnswer: '9.8 meters per second squared'
+  },
+  {
+    questionNumber: 2,
+    catagory: 'Entertainment',
+    question: 'Who played Colonel Jack O\'Neill on the TV show Stargate SG1?',
+    possibleAnswers: [
+      'Christphoer Judge',
+      'Michael Shanks',
+      'Don S. Davis',
+      'Richard Dean Anderson'
+    ],
+    correctAnswer: 'Richard Dean Anderson'
+  },
+  {
+    questionNumber: 3,
+    catagory: 'Art',
+    question: 'Rembrandt painted "Starry Night"',
+    possibleAnswers: [
+      'True',
+      'False'
+    ],
+    correctAnswer: 'False, Van Gogh painted "Starry Night".'
+  }
+];
