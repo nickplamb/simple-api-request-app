50b42d6cbdfa3a8b2cf3a902e7a458ccdb431104
Nicholas Lamb <nickplamb@gmail.com>
2021-03-08 17:09:26 -0500

    1.5 Change for to foreach and move questions to an IIFE with add and getAll functions

--- js/script.js
+++ js/script.js
@@ -1,50 +1,80 @@
-triviaRound = [
-  {
-    questionNumber: 1,
-    catagory: 'science',
-    question: 'What is the acceleration due to gravity on earth?',
-    possibleAnswers: [
-      '9.8 meters per second',
-      '9.8 meters per second squared',
-      '7.2 meters per second squared',
-      '7.2 meters per second'
-    ],
-    correctAnswer: '9.8 meters per second squared'
-  },
-  {
-    questionNumber: 2,
-    catagory: 'Entertainment',
-    question: 'Who played Colonel Jack O\'Neill on the TV show Stargate SG1?',
-    possibleAnswers: [
-      'Christphoer Judge',
-      'Michael Shanks',
-      'Don S. Davis',
-      'Richard Dean Anderson'
-    ],
-    correctAnswer: 'Richard Dean Anderson'
-  },
-  {
-    questionNumber: 3,
-    catagory: 'Art',
-    question: 'Rembrandt painted "Starry Night".',
-    possibleAnswers: [
-      'True',
-      'False'
-    ],
-    correctAnswer: 'False'
-  }
-];
-// Begin the round of trivia
+let triviaRound = (function(){
+  // Array of question objects with answers.
+  let triviaQuestions = [{
+      catagory: 'science',
+      question: 'What is the acceleration due to gravity on earth?',
+      possibleAnswers: [
+        '9.8 meters per second',
+        '9.8 meters per second squared',
+        '7.2 meters per second squared',
+        '7.2 meters per second'
+      ],
+      correctAnswer: '9.8 meters per second squared'
+    },
+    {
+      catagory: 'Entertainment',
+      question: 'Who played Colonel Jack O\'Neill on the TV show Stargate SG1?',
+      possibleAnswers: [
+        'Christphoer Judge',
+        'Michael Shanks',
+        'Don S. Davis',
+        'Richard Dean Anderson'
+      ],
+      correctAnswer: 'Richard Dean Anderson'
+    },
+    {
+      catagory: 'Art',
+      question: 'Rembrandt painted "Starry Night".',
+      possibleAnswers: [
+        'True',
+        'False'
+      ],
+      correctAnswer: 'False'
+    }];
 
-document.write('<main><section class="card-container"><h2>Round 1</h2>')
+  // Retrieve all Questions for this round.
+  function getAll() {
+    return triviaQuestions;
+  }
+  // Add a new questions to this round.
+  function add(newQuestion) {
+    if (typeof newQuestion === 'object') {
+      triviaQuestions.push(newQuestion);
+    } else {
+      console.log('nothing was added')
+    }
+  }
+
+  // Return object with call to getAll and add functions
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+// New question to be added to triviaRound
+let newQuestion = {
+  catagory: 'Entertainment: Music',
+  question: 'The cover of The Beatles album &quot;Abbey Road&quot; featured a Volkswagen Beetle in the background.',
+  possibleAnswers: [
+    'True',
+    'False'
+  ],
+  correctAnswer: 'True'
+}
+
+// Add the newQuestion
+triviaRound.add(newQuestion);
+
+// Begin the round of trivia
+document.write('<main><section class="card-container"><h2>Round 1</h2>');
 
 // Loop through each question in the round
-for (let i = 0; i < triviaRound.length; i++) {
-  // Initialize variable with question for cleaner code
-  const question = triviaRound[i];
-  document.write('<div class="card-container__card"')
-  // Display the question in an h2 tag
-  document.write(`<h3>Question ${question.questionNumber}:</h3>`);
+triviaRound.getAll().forEach((question, index) => {
+  document.write('<div class="card-container__card"');
+
+  // Display the question in an h3 tag
+  document.write(`<h3>Question ${index +1}:</h3>`);
   document.write(`<p>${question.question}</p>`);
 
   // Display list of possible questions
@@ -52,16 +82,28 @@ for (let i = 0; i < triviaRound.length; i++) {
   document.write('<ul>');
 
   // Loop through and display each possible answer
-  for (let x = 0; x < question.possibleAnswers.length; x++) {
-    const possibleAnswer = question.possibleAnswers[x];
+  question.possibleAnswers.forEach((possibleAnswer, index) => {
 
     // Highlight to the correct answer using correctAnswer class with css. Just because it was not easy enough.
     if (possibleAnswer === question.correctAnswer) {
-      document.write(`<li><input type="button" class="btn-answer btn-correctAnswer" value="${x + 1}: ${possibleAnswer}"></li>`)
+      document.write(`<li><input type="button" class="btn-answer btn-correctAnswer" value="${index + 1}: ${possibleAnswer}"></li>`);
     } else {
-      document.write(`<li><input type="button" class="btn-answer" value="${x + 1}: ${possibleAnswer}"></li>`);
+      document.write(`<li><input type="button" class="btn-answer" value="${index + 1}: ${possibleAnswer}"></li>`);
     }
-  }
+  });
   document.write('</ul></div>');
-}
+});
 document.write('</section></main>')
+
+
+
+
+// // Doesnt work yet.  question.correctAnswer is out of scope.
+// function loopThroughPossibleAnswers(possibleAnswer, index) {
+//   // Highlight to the correct answer using correctAnswer class with css. Just because it was not easy enough.
+//   if (possibleAnswer === question.correctAnswer) {
+//     document.write(`<li><input type="button" class="btn-answer btn-correctAnswer" value="${index + 1}: ${possibleAnswer}"></li>`);
+//   } else {
+//     document.write(`<li><input type="button" class="btn-answer" value="${index + 1}: ${possibleAnswer}"></li>`);
+//   }
+// }
