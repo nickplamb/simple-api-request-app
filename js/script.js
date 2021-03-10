32e67b0da4805f80b3ac1eb38a3f680242ea2c54
Nicholas Lamb <nickplamb@gmail.com>
2021-03-10 15:37:57 -0500

    1.6 add styling, flip functionality, event handlers for answers, and correct selection logic

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ let triviaRound = (function(){
         console.error('Object does not have all required properties');
       }
     } else {
-      console.error('Not an object')
+      console.error('Not an object');
     }
   }
 
@@ -59,8 +59,6 @@ let triviaRound = (function(){
       return false;
     }
     // Compare keys one by one.
-
-    
     for (let i = 0; i < template.length; i++) {
       if (arr1[i] !== template[i]) {
         return false;
@@ -71,45 +69,107 @@ let triviaRound = (function(){
   }
 
   // Creates a div.card-container__card with the question and a ul of buttons as answers.
-  function addListItem(question, index) {
+  function addListItem(question, questionIndex) {
     // Container for each card
     let card = document.createElement('li');
     card.classList.add('card-container__card');
-    card.classList.add('question-card__' + (index + 1));
+    card.classList.add('question-card__' + (questionIndex + 1));
     
+    // --------------Card Structure-------------
+    // Card inner
+    let cardInner = document.createElement('div');
+    cardInner.classList.add('card-inner', 'card');
+    // card front. Shows question with list of answers
+    let cardFront = document.createElement('div');
+    cardFront.classList.add('card-front');
+    // Card back. Will flip over when answer selected and show right or wrong.
+    let cardBack = document.createElement('div');
+    cardBack.classList.add('card-back');
+    cardBack.setAttribute('id', 'card-back__'+(questionIndex + 1))
+
+    // --------------FRONT-------------------
     // Header
-    let cardHeader = document.createElement('h3');
-    cardHeader.innerText = 'Question ' + (index + 1) + ':';
-    
+    let cardFrontHeader = document.createElement('h3');
+    cardFrontHeader.innerText = 'Question ' + (questionIndex + 1) + ':';
     // Question
     let cardQuestion = document.createElement('p');
     cardQuestion.innerText = question.question;
-
     // Answer List
     let answerList = document.createElement('ul');
     answerList.classList.add('answer-list');
 
+    card.appendChild(cardInner);
 
-    card.appendChild(cardHeader);
-    card.appendChild(cardQuestion);
-    card.appendChild(answerList);
+    // Fill card Front
+    cardFront.appendChild(cardFrontHeader);
+    cardFront.appendChild(cardQuestion);
+    cardFront.appendChild(answerList);
+
+    cardInner.appendChild(cardFront);
+    cardInner.appendChild(cardBack);
+
 
     // Loop through possible answers, create li with button for each
     // Add each li to .answer-list ul
-    question.possibleAnswers.forEach((possibleAnswer, index) => {
+    question.possibleAnswers.forEach((possibleAnswer, answerIndex) => {
       let answerListItem = document.createElement('li');
       
       // Create Button with class and possible answer
       let answerbtn = document.createElement('button');
+      // create unique button ID
+      let btnId = 'btn-' + questionIndex + '-' + answerIndex;
+
+      // set btn attributes, text, and value
       answerbtn.classList.add('btn-answer');
+      answerbtn.setAttribute('id', btnId);
       answerbtn.innerText = possibleAnswer;
-      answerbtn.value = index+1;
+      answerbtn.value = answerIndex+1;
+
+      // Create event handler for each button
+      createEventHandler(btnId, possibleAnswer);
 
       // add button to li
       answerListItem.appendChild(answerbtn);
-
+      // add li to anser list ul
       answerList.appendChild(answerListItem);
     });
+
+    function createEventHandler(btnId, possibleAnswer) {
+      let parentCard = document.querySelector('.card-container')
+
+      if (parentCard.addEventListener) {
+        parentCard.addEventListener('click', e => handler(e, btnId, question, possibleAnswer), false);
+      }else if (parentCard.attachEvent) {
+        parentCard.attachEvent('onclick', e => handler(e, btnId));
+      }
+    }
+
+    function handler(e, btnId, question, possibleAnswer) {
+      e.preventDefault();
+      // I think this isolates the handler to just the one clocked.
+      if (e.target.id == btnId) { //http://jsfiddle.net/H97WY/
+        let buttonSelected = document.querySelector('#'+btnId)
+        let answerSelected = buttonSelected.innerText
+        let questionNumber = btnId.split('-')[1];
+
+        createCardBack(answerSelected, questionNumber);
+      }
+    }
+
+    function createCardBack(answerSelected, questionNumber) {
+      let cardBackHeader = document.createElement('h3');
+      // cardBackHeader.classList.add('');
+
+      // Determine if the answer selected is correct.
+      if (answerSelected === question.correctAnswer) {
+        cardBackHeader.innerText = 'You are so smart!'
+      } else {
+        cardBackHeader.innerText = 'Isn\'t that cute. BUT IT\'S WRONG!'
+      }
+      
+      cardBack.appendChild(cardBackHeader);
+      cardInner.classList.add('flip-over');
+    }
 
     return card;
   }
@@ -133,6 +193,8 @@ let newQuestion = {
   correctAnswer: 'True',
 }
 
+
+
 // Add the newQuestion
 triviaRound.add(newQuestion);
 
@@ -148,17 +210,67 @@ allQuestions.forEach((question, index) => {
 });
 
 function decodeBase64(msg) {
-  return atob(msg);
+  let decodedObject = new Object;
+  // msg.entries().forEach
+//  console.log(msg);
+  // return decodedObject;
 }
 
 // -------Event listeners-----
+let optionsBtn = document.querySelector('button.start-form__item');
+// optionsBtn.addEventListener('click', getQuestionsFromOpenDB);
 
-
-
-function checkAnswer(slectedChoice, ) {
-  if (choice === question.correctAnswer) {
-    
-  } else {
-    
-  }
+let parentCard = document.querySelector
+if (parent.addEventListener) {
+    parent.addEventListener('click', handler, false);
+}else if (parent.attachEvent) {
+    parent.attachEvent('onclick', handler);
 }
+
+function handler(e) {
+    if (e.target.id == 'test') {
+         // the button was clicked
+    }
+}
+
+
+
+
+
+
+
+function getQuestionsFromOpenDB() {
+  let url = 'https://opentdb.com/api.php?encode=base64';
+
+  let query = new Object;
+  query.amount = document.querySelector('#num-of-questions').value;
+  query.category = document.querySelector('#categories').value;
+  query.difficulty = document.querySelector('#difficulties').value;
+  query.type = document.querySelector('#question-type').value;
+
+
+  Object.entries(query).forEach(option => {
+    if (option[1] !== '') {
+      url += '&' + option[0] + '=' + option[1];
+    }
+  });
+
+  let encodedQuestions = fetchAndParseQuestions(url);
+  let decodedQuestions = decodeBase64(encodedQuestions);
+
+}
+
+function fetchAndParseQuestions(url) {
+  let encodedQuestions = fetch(url).then(response => {
+    return response.json();
+  }).then(data => {
+    return data;
+  });
+  // .catch(err => {
+  //   console.warn('something went wrong. ' + err);
+  // });
+  console.log(encodedQuestions);
+  return encodedQuestions.results;
+}
+
+
