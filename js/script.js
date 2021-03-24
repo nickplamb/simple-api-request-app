a4fc4f25037b7420777176ff733662384d5c1f72
Nicholas Lamb <nickplamb@gmail.com>
2021-03-24 17:42:26 -0400

    add eslint and prettier to project. fix issues.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Wrap main portion of the app in a Revealing Module Pattern
-let triviaSession = (function(){
-
+let triviaSession = (function () {
   // Array of question objects with answers.
   let triviaQuestions = [];
   let allCategories = [];
@@ -10,7 +9,14 @@ let triviaSession = (function(){
 
   let apiBaseUrl = 'https://opentdb.com/api.php?encode=base64';
   // Array of required properties for each question
-  const questionObjectTemplate = ['category', 'type', 'difficulty', 'question', 'correct_answer', 'incorrect_answers']
+  const questionObjectTemplate = [
+    'category',
+    'type',
+    'difficulty',
+    'question',
+    'correct_answer',
+    'incorrect_answers',
+  ];
   const snarkyResponses = {
     correct: [
       'Correct! Looks like you are a pretty good guesser.',
@@ -19,7 +25,7 @@ let triviaSession = (function(){
       'Correct! You might just be smarter then a child.',
       'Correct! You had a 50/50 chance though.',
       'Correct. Everyone knows that though.',
-      'Guessing, 50% of the time it works every time.'
+      'Guessing, 50% of the time it works every time.',
     ],
     incorrect: [
       'So close...(not really.)',
@@ -29,31 +35,32 @@ let triviaSession = (function(){
       'Do you have a hard time remembering to breath?',
       'My dog knew that one...',
       'That was a no brainer.',
-    ]
-  }
+    ],
+  };
 
   // Ajax request to API for all categories. push to allCategories, call populateCategoriesDropdown
   function fetchCategories() {
     let categoriesURL = 'https://opentdb.com/api_category.php';
-    return $.ajax(categoriesURL, { dataType: 'json' }
-    ).then(responseJSON => {
-      responseJSON.trivia_categories.forEach(category => {
-        let categoryListItem = {
-          id: category.id,
-          name: category.name
-        }
-        allCategories.push(categoryListItem);
+    return $.ajax(categoriesURL, { dataType: 'json' })
+      .then((responseJSON) => {
+        responseJSON.trivia_categories.forEach((category) => {
+          let categoryListItem = {
+            id: category.id,
+            name: category.name,
+          };
+          allCategories.push(categoryListItem);
+        });
+        populateCategoriesDropdown();
+      })
+      .catch((e) => {
+        console.error(e);
       });
-      populateCategoriesDropdown();
-    }).catch(e => {
-      console.error(e);
-    });
   }
 
   // Add all categories to categories dropdown on form.
   function populateCategoriesDropdown() {
     let dropdown = $('#categories');
-    allCategories.forEach(item => {
+    allCategories.forEach((item) => {
       let listItem = $(`<option value="${item.id}">${item.name}</option>`);
       dropdown.append(listItem);
     });
@@ -66,45 +73,46 @@ let triviaSession = (function(){
 
     let url = apiBaseUrl;
 
-    let query = new Object;
+    let query = new Object();
     query.amount = $('#num-of-questions').val();
     query.category = $('#categories').val();
     query.difficulty = $('#difficulties').val();
     query.type = $('#question-type').val();
 
-    Object.entries(query).forEach(option => {
+    Object.entries(query).forEach((option) => {
       if (option[1] !== '') {
         url += `&${option[0]}=${option[1]}`;
       }
     });
-    fetchQuestions(url)
+    fetchQuestions(url);
   }
 
   function fetchQuestions(url) {
-    $.ajax(url, {dataType: 'json'}
-    ).then(responseJSON => {
-      if (responseJSON.results.length !== 0) {
-        responseJSON.results.forEach(item => {
-          let question = {
-            category: decodeBase64(item.category),
-            type: decodeBase64(item.type),
-            difficulty: decodeBase64(item.difficulty),
-            question: decodeBase64(item.question),
-            correct_answer: decodeBase64(item.correct_answer),
-            incorrect_answers: decodeBase64(item.incorrect_answers)
-          }
-          addQuestionToArray(question);
-        });
-      } else {
-        // console.error('Too Specific');
-        let title = 'No questions found.';
-        let text = 'Please change your options and try again.';
-        showDialog(title, text, 'optionError');
-      }
-      displayQuestions();
-    }).catch(e => {
-      console.error(e);
-    });
+    $.ajax(url, { dataType: 'json' })
+      .then((responseJSON) => {
+        if (responseJSON.results.length !== 0) {
+          responseJSON.results.forEach((item) => {
+            let question = {
+              category: decodeBase64(item.category),
+              type: decodeBase64(item.type),
+              difficulty: decodeBase64(item.difficulty),
+              question: decodeBase64(item.question),
+              correct_answer: decodeBase64(item.correct_answer),
+              incorrect_answers: decodeBase64(item.incorrect_answers),
+            };
+            addQuestionToArray(question);
+          });
+        } else {
+          // console.error('Too Specific');
+          let title = 'No questions found.';
+          let text = 'Please change your options and try again.';
+          showDialog(title, text, 'optionError');
+        }
+        displayQuestions();
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   }
 
   // validate question
@@ -141,22 +149,40 @@ let triviaSession = (function(){
     possibleAnswers.push(question.correct_answer);
 
     // Create elements for card structure.
-    let cardRow = $('<div></div').addClass('row justify-content-center card-container__row')
-    let card = $('<div></div>').addClass('card-container__card col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 my-3');
-    let questionNumberClass = 'question-' + (questionIndex+1);
-    let cardInner = $('<div></div>').addClass('card card-inner bg-light ' + questionNumberClass);
+    let cardRow = $('<div></div').addClass(
+      'row justify-content-center card-container__row'
+    );
+    let card = $('<div></div>').addClass(
+      'card-container__card col-11 col-sm-10 col-md-8 col-lg-7 col-xl-5 my-3'
+    );
+    let questionNumberClass = 'question-' + (questionIndex + 1);
+    let cardInner = $('<div></div>').addClass(
+      'card card-inner bg-light ' + questionNumberClass
+    );
     let cardFront = $('<div></div>').addClass('card-front card-body bg-light');
     let cardHeader = $('<div></div>').addClass('card-header bg-warning mt-3');
-    let cardHeaderTitle = $('<h4></h4>').addClass('card-title text-center').text(`Question ${questionIndex+1}:`);
-    let cardHeaderText = $('<p></p>').addClass('card-text').text(question.question);
-    let answerList = $('<div></div>').addClass('mt-4 answer-btns').attr('id', 'q-answers-'+(questionIndex+1));
-    let cardBack = $('<div></div>').addClass('card-back card-body bg-light ' + questionNumberClass);
+    let cardHeaderTitle = $('<h4></h4>')
+      .addClass('card-title text-center')
+      .text(`Question ${questionIndex + 1}:`);
+    let cardHeaderText = $('<p></p>')
+      .addClass('card-text')
+      .text(question.question);
+    let answerList = $('<div></div>')
+      .addClass('mt-4 answer-btns')
+      .attr('id', 'q-answers-' + (questionIndex + 1));
+    let cardBack = $('<div></div>').addClass(
+      'card-back card-body bg-light ' + questionNumberClass
+    );
     let cardBackResult = $('<div></div>').addClass('mt-4');
-    let cardBackAnswer = $('<p></p>').addClass('card-text card-result ' + questionNumberClass);
+    let cardBackAnswer = $('<p></p>').addClass(
+      'card-text card-result ' + questionNumberClass
+    );
     let cardBackImg = $('<img>').addClass(questionNumberClass + ' result-img');
 
     // Attach event handler to the answerList element as a delegate.
-    answerList.on('click', 'button', e => answerHandler(e, question, numOfQuestions, questionNumberClass));
+    answerList.on('click', 'button', (e) =>
+      answerHandler(e, question, numOfQuestions, questionNumberClass)
+    );
 
     // Card Structure
     // cardRow
@@ -190,46 +216,48 @@ let triviaSession = (function(){
         .attr('id', btnId)
         .text(possibleAnswers)
         .val(answerIndex + 1);
-      
-        answerArr.push(answerButton);
-    })
+
+      answerArr.push(answerButton);
+    });
 
     shuffleArray(answerArr);
 
-    answerArr.forEach(answerBtn => {
+    answerArr.forEach((answerBtn) => {
       answerList.append(answerBtn);
-    })
+    });
   }
 
   // Called by event Listener, changes text and image on back of card based on answer. Flips over card.
   function answerHandler(e, question, numOfQuestions, questionNumberClass) {
     questionsAnswered++;
     let selectedAnswer = e.target.innerText;
-    if(selectedAnswer === question.correct_answer) {
-      score++
-      $(`.card-result.${questionNumberClass}`).text(`The correct answer is: ${question.correct_answer}`);
-      $(`img.${questionNumberClass}`)
-        .attr({
-          src: 'img/correct_icon.svg',
-          alt: 'Your answer was correct!'
-        });
-    } else{
-      $(`.card-result.${questionNumberClass}`).text(`The correct answer is: ${question.correct_answer}`);
-      $(`img.${questionNumberClass}`)
-        .attr({
-          src: 'img/wrong_icon.svg',
-          alt: 'Your answer was incorrect'
-        });
+    if (selectedAnswer === question.correct_answer) {
+      score++;
+      $(`.card-result.${questionNumberClass}`).text(
+        `The correct answer is: ${question.correct_answer}`
+      );
+      $(`img.${questionNumberClass}`).attr({
+        src: 'img/correct_icon.svg',
+        alt: 'Your answer was correct!',
+      });
+    } else {
+      $(`.card-result.${questionNumberClass}`).text(
+        `The correct answer is: ${question.correct_answer}`
+      );
+      $(`img.${questionNumberClass}`).attr({
+        src: 'img/wrong_icon.svg',
+        alt: 'Your answer was incorrect',
+      });
     }
 
-    if (questionsAnswered === numOfQuestions){
+    if (questionsAnswered === numOfQuestions) {
       let title = '';
       let text = '';
       if (score === 0) {
         title = 'You are not very good at this.';
-        text = 'You didn\'t get any correct.';
+        text = "You didn't get any correct.";
         // totalScoreElement.innerText = 'You are not very good at this. You didn\'t get any correct.';
-      } else if (score <= numOfQuestions * 0.6){
+      } else if (score <= numOfQuestions * 0.6) {
         title = 'Was that the best you could do?';
         text = 'You only got ' + score + ' out of ' + numOfQuestions + '.';
         // totalScoreElement.innerText = 'You gave it your best shot. You only got ' + score + ' out of ' + numOfQuestions + '.';
@@ -244,7 +272,7 @@ let triviaSession = (function(){
       }
 
       setTimeout(() => {
-        showDialog(title, text, 'endOfRound')
+        showDialog(title, text, 'endOfRound');
       }, 2000);
     }
     $('.card-inner.' + questionNumberClass).addClass('flip-over');
@@ -278,7 +306,7 @@ let triviaSession = (function(){
   function decodeBase64(msg) {
     if (typeof msg === 'object') {
       let msgArr = [];
-      msg.forEach(item => {
+      msg.forEach((item) => {
         msgArr.push(atob(item));
       });
       return msgArr;
@@ -307,33 +335,29 @@ let triviaSession = (function(){
   // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
   function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
   }
 
   // Reset questions and score for next round
   function resetRound() {
-
     $('.card-container__row').remove();
     triviaQuestions = [];
     score = 0;
     questionsAnswered = 0;
   }
 
-
   // -----------return needed functions to outside IIFE---------------------------
   return {
     fetchCategories: fetchCategories,
     prepareQuestionRetrieval: prepareQuestionRetrieval,
-    resetRound: resetRound
+    resetRound: resetRound,
   };
-
 })();
 
 triviaSession.fetchCategories();
-
-$('#startForm').on('submit', e => triviaSession.prepareQuestionRetrieval(e));
+$('#startForm').on('submit', (e) => triviaSession.prepareQuestionRetrieval(e));
 $('#restRoundButton').on('click', () => triviaSession.resetRound());
